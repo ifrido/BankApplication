4fbbd085438d828c8355e71624b8db158959841d
MasterFun <christian.kiss12@gmail.com>
2014-02-03 21:52:14 +0100

    Prepared controller for scene switching NOT WORKING YET

--- BankApplication/src/application/BankController.java
+++ BankApplication/src/application/BankController.java
@@ -1,24 +1,40 @@
 package application;
 
 
+import java.io.IOException;
+
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class BankController {
+	private Bank bank = Bank.getInstance();
 	
+/*	Main app;
 	
-	// Login
+
+	public void setApp(Main app) {
+		this.app = app;
+	}*/
+
+	// Login	
 	@FXML
 	TextField loginUser;
 	@FXML
 	PasswordField loginPassword;
-	public void login(){
-		loginUser.getText();
+	public void login(){		
+		Kunde user = bank.login(loginUser.getText(), loginPassword.getText());
 		System.out.println(loginUser.getText());
+		//app.switchScene(Main.USER);
 	}
+	
 	
 	
 	
--- BankApplication/src/application/Main.java
+++ BankApplication/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -15,19 +17,44 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public final static String ADMIN = "ADMIN";
+	public final static String USER = "USER";
+	Scene scene;
+	AnchorPane root;
+	/*AnchorPane admin;
+	AnchorPane user;*/
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			//AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("InterfaceUser.fxml"));
-			Scene scene = new Scene(root,900,600);
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			/*user = (AnchorPane)FXMLLoader.load(getClass().getResource("InterfaceUser.fxml"));
+			admin = (AnchorPane)FXMLLoader.load(getClass().getResource("InterfaceAdmin.fxml"));*/
+			scene = new Scene(root,900,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			/*BankController bankController = new BankController();
+			fxmlLoader.setController(bankController);
+			bankController.setApp(this);*/
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}  
+	
+	/*public void switchScene(String sceneName) {
+		switch (sceneName) {	
+		case ADMIN:
+			scene.setRoot(admin);
+			break;
+		case USER:
+			scene.setRoot(user);
+		default: 
+			scene.setRoot(root);
+			break;
+		}
+	}*/
 	
 	public static void main(String[] args) {
 		launch(args);
