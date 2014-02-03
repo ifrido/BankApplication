cc0a29911687efbcaa6a1bc069e9701af1c66c80
MasterFun <christian.kiss12@gmail.com>
2014-02-03 19:05:27 +0100

    Tolle neue GUI

--- BankApplication/src/application/BankController.java
+++ BankApplication/src/application/BankController.java
@@ -1,5 +1,65 @@
 package application;
 
+import javax.swing.MenuElement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class BankController {
+	
+	
+	// Login
+	@FXML
+	TextField loginUser;
+	PasswordField loginPassword;
+	public void login(){
+		loginUser.getText();
+		System.out.println(loginUser.getText());
+	}
+	
+	
+	
+	
+	
+	
+	@FXML
+	Pane userKonten, userZahlungen, userEdit;
+	@FXML
+	Accordion menue;
+
+	
+
+	
+	
+	
+	// Menü Userinterface Christian Kiss
+
+	
+	public void menueAction(){
+//		
+//		
+//		userKonten.setVisible(false);
+//		userZahlungen.setVisible(false);
+//		userEdit.setVisible(false);
+		userEdit.setVisible(true);
+		
+//		
+//		System.out.println(menue.get);
+//
+//		
+////		if (menu.equals( "benutzerverwaltung" )){
+////			benutzerverwaltung.setVisible(true);	
+////		}
+//		
+
+	}
+	
+	
+	
+	
+	
 	
 }
--- BankApplication/src/application/Main.java
+++ BankApplication/src/application/Main.java
@@ -18,8 +18,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("InterfaceUser.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			//AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("InterfaceUser.fxml"));
 			Scene scene = new Scene(root,900,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
